7afe450690ead82e8d35321fc2d27c91110757e9
kolson463 <kdolson111@gmail.com>
2022-07-19 14:55:00 -0500

    first commit

---
+++ todo-app/src/components/TodoForm.svelte
@@ -0,0 +1,28 @@
+<script>
+    let todo = '';
+
+    import {addTodo} from "../storage/todoStorage.js"
+    const handleSubmit = () => {
+        addTodo(todo);
+        todo = ''
+        console.log("submitting")
+    }
+
+
+</script>
+
+<form class="my-6" on:submit|preventDefault={handleSubmit}>
+	<div class="flex flex-col text-sm mb-2">
+		<label class="font-bold mb-2 text-gray-800" for="todo">Todo</label>
+		<input
+			class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"
+			type="text"
+			name="todo"
+			placeholder="Enter todo!"
+            bind:value={todo}
+		/>
+	</div>
+	<button class="w-full bg-blue-500 hover:bg-blue-600 text-white rounded shadow-sm" type="submit"
+		>Submit</button
+	>
+</form>
---
+++ todo-app/src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+// @ts-nocheck
+
+	import TodoForm from '../components/TodoForm.svelte';
+	import Todo from '../components/Todo.svelte';
+	import { todos } from '../storage/todoStorage.js';
+</script>
+
+<main>
+	<h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">My Todos</h1>
+	<TodoForm />
+	{#each $todos as todo}
+		<Todo {todo} index={todos.id} />
+	{/each}
+</main>
