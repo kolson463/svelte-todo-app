4fa864afe0fafdb7565360b42f9a3ee5ec65470b
kolson463 <98200634+kolson463@users.noreply.github.com>
2022-07-25 16:59:49 -0500

    Added dark mode toggle

--- todo-app/src/components/TodoForm.svelte
+++ todo-app/src/components/TodoForm.svelte
@@ -15,17 +15,19 @@
 </script>
 
 <form class="my-6" on:submit|preventDefault={handleSubmit}>
-	<div class="flex flex-col text-sm mb-2">
-		<label class="font-bold mb-2 text-gray-800" for="todo">Todo</label>
+	<div class="flex flex-col text-sm mb-2 mx-2">
+		<label class="font-bold mb-2  " for="todo">New Todo</label>
 		<input
-			class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"
+			class="text-black appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"
 			type="text"
 			name="todo"
 			placeholder="Enter todo!"
             bind:value={todo}
 		/>
 	</div>
-	<button class="w-full bg-blue-500 hover:bg-blue-600 text-white rounded shadow-sm" type="submit"
+	<div class="mx-2">
+	<button class=" w-full bg-blue-500 hover:bg-blue-600 text-white rounded shadow-sm" type="submit"
 		>Submit</button
 	>
+	</div>
 </form>
--- todo-app/src/routes/index.svelte
+++ todo-app/src/routes/index.svelte
@@ -1,15 +1,42 @@
 <script>
 // @ts-nocheck
+import DarkMode from "svelte-dark-mode";
+
+import { afterUpdate } from "svelte";
 
 	import TodoForm from '../components/TodoForm.svelte';
 	import Todo from '../components/Todo.svelte';
 	import { todos } from '../storage/todoStorage.js';
+
+let theme;
+
+
+afterUpdate(() => {
+	document.body.className = theme; // "dark" or "light"
+  });
+
+
+
 </script>
 
+<DarkMode bind:theme />
+
 <main>
-	<h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">My Todos</h1>
+<button class="border-2 rounded-lg p-3 font-semibold" type="menu" on:click={() => (theme ==="light" ? theme = "dark" : theme = "light"
+)}
+	>
+  {theme ==="light" ? "Toggle Dark Mode" : "Toggle Light Mode"}
+</button>
+	<h1 class={`text-2xl font-bold text-center my-2 md:text-3xl ${theme === "light" ? 'text-gray-800 '  : 'text-white'}`}>My Todos</h1>
 	<TodoForm />
 	{#each $todos as todo}
 		<Todo {todo} index={todos.id} />
 	{/each}
 </main>
+
+<style>
+	:global(.dark) {
+	  background: #032f62;
+	  color: #f1f8ff;
+	}
+  </style>
